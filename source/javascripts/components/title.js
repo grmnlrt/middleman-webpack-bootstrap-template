b3021e20f74abffc4efb94e32f6bc874bf745b1f
Germain Loret <germain.loret@gmail.com>
2018-04-04 22:10:55 +0200

    Update title.js

--- source/javascripts/components/title.js
+++ source/javascripts/components/title.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Middleman is ready", "Webpack too"],
+    strings: ["Bootstrap is ready", "Webpack too"],
     typeSpeed: 50,
     loop: true
   });
